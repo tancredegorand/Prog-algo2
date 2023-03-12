4e4a993010b2bc5f5c7532b515461df476f28c19
firegreen <steeve.v91@gmail.com>
2023-03-13 00:20:15 +0100

    * add: custom_msleep

--- lib/utils.h
+++ lib/utils.h
@@ -2,9 +2,10 @@
 #define UTILS_H
 
 #include <QThread>
-
 #include <QMutex>
 #include <QWaitCondition>
+#include <cmath>
+#include <chrono>
 
 inline void custom_msleep(int milliseconds)
 {
@@ -13,5 +14,14 @@ inline void custom_msleep(int milliseconds)
     QMutexLocker locker(&mutex);
     wc.wait(&mutex, milliseconds);
 }
+
+inline void custom_msleep(std::chrono::milliseconds period)
+{
+    QWaitCondition wc;
+    QMutex mutex;
+    QMutexLocker locker(&mutex);
+    wc.wait(&mutex, period.count());
+}
+
 
 #endif // UTILS_H
